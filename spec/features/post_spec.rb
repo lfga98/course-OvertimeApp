89bb2cb41527f560da8eba7a623201ae888d96b9
lfga98 <lfgarciauaz@gmail.com>
2018-06-26 14:04:24 -0500

    Created index action for posts

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,11 @@
+# spec/features/static_spec.rb
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'index' do
+    it 'can be reached successfully' do
+      visit posts_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
