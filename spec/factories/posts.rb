a278ea87607c552c5f9e17df151a5cae8f125dfd
lfga98 <lfgarciauaz@gmail.com>
2018-07-04 14:19:24 -0500

    Implemented overtiem request on the creation and the index, field created in the database

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,11 +3,13 @@ FactoryBot.define do
     date Date.today
     rationale "Some Rationale"
     user
+    overtime_request 3.5
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
     user
+    overtime_request 7.5
   end
 end
