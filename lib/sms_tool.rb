174445c29b59f1d8474bbb9117e61e1d38cc6167
lfga98 <lfgarciauaz@gmail.com>
2018-07-05 15:55:38 -0500

    Integrated the sms ability

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,13 @@
 module SmsTool
-  def self.send_sms(number:, message:)
-    puts "Sending SMS..."
-    puts "#{message} to #{number}"
-  end
+	account_sid = ENV['TWILIO_SID']
+  auth_token = ENV['TWILIO_AUTH']
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+	def self.send_sms(number:, message:)
+		@client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: "+1#{number}",
+      body: "#{message}"
+    )
+	end
 end
