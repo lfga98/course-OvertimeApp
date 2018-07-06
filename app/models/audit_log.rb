96960fd1621a45eb9d41bd10a074dfda571bb9ff
lfga98 <lfgarciauaz@gmail.com>
2018-07-06 16:52:56 -0500

    Added the enum to Auditlog model and colored labels

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,14 +1,10 @@
 class AuditLog < ActiveRecord::Base
+  enum status:{pending: 0,confirmed: 1}
   belongs_to :user
-
   validates_presence_of :user_id, :status, :start_date
-
   after_initialize :set_defaults
-
-
   private
-
-    def set_defaults
+  def set_defaults
       self.start_date ||= Date.today - 6.days
     end
 end
