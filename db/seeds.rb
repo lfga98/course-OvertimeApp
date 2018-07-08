47f862895a74a3e1bc29c23c30c75845cc5066e9
lfga98 <lfgarciauaz@gmail.com>
2018-07-08 14:52:04 -0500

    Implememnted the employee homepage, styled and added 3 buttons for confirmation

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,27 +3,27 @@
 										password_confirmation: "asdfasdf",
 										first_name: "Jon",
 										last_name: "Snow",
-										phone:"4921509646")
+										phone: "4322386131")
 
 puts "1 User created"
 
-
 AdminUser.create(email: "admin@test.com",
-								password: "asdfasdf",
-								password_confirmation: "asdfasdf",
-								first_name: "Admin",
-								last_name: "Sir",
-								phone:"4921509646")
-puts "1 AdminUser created"
+									password: "asdfasdf",
+									password_confirmation: "asdfasdf",
+									first_name: "Admin",
+									last_name: "Name",
+									phone: "4322386131")
+
+puts "1 Admin User created"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id,overtime_request: 4.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
 
-100.times do |auditlog|
-	AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today-6.days))
-end
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
-puts "100 audit logs have been created"
+puts "3 audit logs have been created"
